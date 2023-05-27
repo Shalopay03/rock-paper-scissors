78e56c17602ff32ee8d3ca215da39a59f86fe6d2
Shalopay03 <134221829+Shalopay03@users.noreply.github.com>
2023-05-27 13:48:55 +0300

    add player prompt and round function

--- scripts.js
+++ scripts.js
@@ -3,8 +3,48 @@ function getRandomInt(max) {
   }
 
 function getComputerChoice(){
-    const rockPaperScissors=["Rock", "Paper", "Scissors"];
+    const rockPaperScissors=["rock", "paper", "scissors"];
     return rockPaperScissors[getRandomInt(3)];
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice(){
+     return prompt("Enter your choice:").toLowerCase();
+}
+
+function round(computerChoice, playerChoice){
+    console.log("Computer: " + computerChoice);
+    console.log("Player: " + playerChoice);
+    if(computerChoice===playerChoice)
+    {
+        return "Draw!";
+    }
+    else if (computerChoice==="rock" && playerChoice==="paper")
+    {
+        return "Player wins!";
+    }
+    else if (computerChoice==="rock" && playerChoice==="scissors")
+    {
+        return "Computer wins!";
+    }
+    else if (computerChoice==="paper" && playerChoice==="rock")
+    {
+        return "Computer wins!";
+    }
+    else if (computerChoice==="paper" && playerChoice==="scissors")
+    {
+        return "Player wins!";
+    }
+    else if (computerChoice==="scissors" && playerChoice==="rock")
+    {
+        return "Player wins!";
+    }
+    else if (computerChoice==="scissors" && playerChoice==="paper")
+    {
+        return "Computer wins!";
+    }
+    else{
+        return "Invalid input! Try again!";
+    }
+}
+
+console.log(round(getComputerChoice(), getPlayerChoice()));
