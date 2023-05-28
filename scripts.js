b6a956ea971d8112510fd978bf4fdbd76fb0df5d
Shalopay03 <134221829+Shalopay03@users.noreply.github.com>
2023-05-28 14:51:28 +0300

    add full functionality to interface

--- scripts.js
+++ scripts.js
@@ -7,18 +7,8 @@ function getComputerChoice(){
     return rockPaperScissors[getRandomInt(3)];
 }
 
-/*function getPlayerChoice(){
-     return prompt("Enter your choice:").toLowerCase();
-}*/
-
 function playRound(computerChoice, playerChoice){
-    console.log("Computer: " + computerChoice);
-    console.log("Player: " + playerChoice);
-    if(computerChoice===playerChoice)
-    {
-        return "draw";
-    }
-    else if (computerChoice==="rock" && playerChoice==="paper")
+    if (computerChoice==="rock" && playerChoice==="paper")
     {
         return "player";
     }
@@ -42,14 +32,69 @@ function playRound(computerChoice, playerChoice){
     {
         return "computer";
     }
-    else{
-        return "invalid";
+    else
+    {
+        return "draw";
     }
+
+}
+
+function givePointTo(whoGainsPoint){
+    const score = document.querySelector(`#${whoGainsPoint}-score-number`);
+    console.log(score);
+    score.textContent = "SCORE - 1";
 }
 
 const buttons = document.querySelectorAll("button");
-buttons.forEach(button => {
+
+let playerScore = 0;
+const playerScoreUpdate = document.querySelector(`#player-score-number`);
+
+let computerScore = 0;
+const computerScoreUpdate = document.querySelector(`#computer-score-number`);
+
+const winnerText=document.querySelector("#winner-text");
+
+const playerImage = document.querySelector("#player-pick");
+const computerImage = document.querySelector("#computer-pick");
+
+const controller = new AbortController();
+
+    buttons.forEach(button => {
     button.addEventListener('click', event => {
-            alert(playRound(getComputerChoice(), event.target.id));
-        });
+        let computerChoice=getComputerChoice();
+        let playerChoice=event.target.id;
+        result=playRound(computerChoice, playerChoice);
+        if(result === "player")
+        {            
+            playerScore++;
+            playerScoreUpdate.textContent = `SCORE - ${playerScore}`;
+            winnerText.textContent = "Player gains a point!";
+        }
+        else if(result === "computer")
+        {            
+            computerScore++;
+            computerScoreUpdate.textContent = `SCORE - ${computerScore}`;
+            winnerText.textContent = "Computer gains a point!";
+        }
+        else
+        {
+            winnerText.textContent = "It's a draw!";
+        }
+
+        
+        playerImage.setAttribute('src', `images/${playerChoice}.png`);
+        computerImage.setAttribute('src', `images/${computerChoice}.png`);
+
+        if (playerScore >= 5) {
+            winnerText.textContent = "You won!";
+            controller.abort()
+        }
+
+        if (computerScore >= 5) {
+            winnerText.textContent = "Computer won!";
+            controller.abort()
+        }
+        
+        }, {signal: controller.signal});
     });
